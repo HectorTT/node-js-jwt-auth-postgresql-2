a4cd0374e88368c98f207ad87c779a56aa6f5b6b
HectorTgh <62454899+HectorTT@users.noreply.github.com>
2022-06-27 15:39:01 -0500

    Logout

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -5,12 +5,26 @@ import { useEffect, useState } from "react";
 
 export default function Navbar() {
     const navigate = useNavigate()
-/*     const [login, setLogin] = useState(false);
-
+     const [login, setLogin] = useState(false);
+/*
     if(localStorage.getItem('token')){
         setLogin(true);
     }
  */
+    const logout = (login) => {
+        setLogin(true);
+    }
+
+    const handleClick = () => {
+        localStorage.removeItem('token');
+        navigate('/login');
+    }
+
+    useEffect(() => {
+        if (localStorage.getItem('token')) {
+            logout(localStorage.getItem('token'));
+        }
+    }, [localStorage.getItem('token')])
   return (
     <>
     <Box sx={{flexGrow:1}}>
@@ -20,11 +34,10 @@ export default function Navbar() {
                     <Typography sx={{flexGrow:1}} variant="h6">
                     <Link  to='/home' style={{textDecoration:"none", color:"black"}}>Home</Link>
                     </Typography>
-                    <Typography>
+                        {login ? <Button onClick={ ()=> handleClick()}>Logout</Button> : <Button onClick={() => {navigate('/login')}}>Login</Button>}
                     {/* {localStorage.getItem('token') ? ( <Link onClick={() => console.log("NO")} style={{textDecoration:"none", color:"black"}}>Logout</Link> ) : ( <Link to='/login' style={{textDecoration:"none", color:"black"}}>Login</Link> )} */}
-                    </Typography>
-                    <Button color='primary' onClick={ ()=> navigate("/login")}>Login</Button>
-                    <Button color='primary' onClick={ ()=> navigate("/signup")}>SignUp</Button>
+                    {/* <Button color='primary' onClick={ ()=> navigate("/login")}>Login</Button> */}
+                    {login ? <Button onClick={ ()=> handleClick()}>Perfil</Button> : <Button color='primary' onClick={ ()=> navigate("/signup")}>SignUp</Button>}
                 </Toolbar>
             </Container>
         </AppBar>
